89f950c84add95f55f4f5bfe3dda81b9cd39142c
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-19 18:31:44 +0600

    email and password authentication 1st part

---
+++ config/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiekONEnCH8XUoxK4IfQ56e-lpCC6zuKU",
+  authDomain: "course-generator-718f0.firebaseapp.com",
+  projectId: "course-generator-718f0",
+  storageBucket: "course-generator-718f0.firebasestorage.app",
+  messagingSenderId: "903512493401",
+  appId: "1:903512493401:web:398acb89860a7d5ef15048",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
