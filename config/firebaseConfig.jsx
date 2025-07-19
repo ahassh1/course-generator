1d17a4127284a3dfb7a3b4d617ee5042f9ab95b1
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-19 18:54:05 +0600

    firebase auth 2nd part

--- config/firebaseConfig.jsx
+++ config/firebaseConfig.jsx
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
