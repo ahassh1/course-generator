049c6d77bcd439c4de7c67204d4427beb18a6fc5
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-19 12:40:42 +0600

    logo added

--- app/auth/signUp.jsx
+++ app/auth/signUp.jsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 50,
+      }}
+    >
+      <Image
+        source={require("./../../assets/images/ahasshLogo.png")}
+        style={{
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
